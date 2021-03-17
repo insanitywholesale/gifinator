248d08fa459b576d4f72e91065745741529857da
lhz <pepitsizous@zoho.com>
2021-03-17 17:57:26 +0200

    try to make script using the docker image, no workie

--- README.md
+++ README.md
@@ -6,23 +6,28 @@ look at TODO for what's left to be done (hint: it's a lot).
 # running (for development)
 the following should be run in the order they are listed and each one in a different terminal
 
-## redis (port 6379) and minio (port 9000)
+## docker
+### all-in-one script
+just run `./rundocker.sh` and then try to access `http://localhost:8090`
+
+## manually
+### redis (port 6379) and minio (port 9000)
 from the root of the repo, run `./rundeps.sh`
 
-## render (port 8080)
+### render (port 8080)
 this one is pretty simple, use `go run render.go`
 
-## gifcreator (worker port 8081, server port 8082)
+### gifcreator (worker port 8081, server port 8082)
 go into its directory and run `./rungifcreator.sh`
 
-## frontend (port 8090)
+### frontend (port 8090)
 also pretty simple, from inside its directory run `./runfront.sh`
 
-## create bucket and load assets
+### create bucket and load assets
 go to http://localhost:9000 and log in using the credentials `minioaccesskeyid` and `miniosecretaccesskey` then create a bucket named `gifbucket` and load all the assets in `gifcreator/scene` to it
 
 ## visit web page
 access http://localhost:8090 using a web browser, fill in the text, select one of the 3 options, click Create and wait
 
 # running (for production)
-there are eventually going to be kubernetes manifests for it in the `infra` repository under `kube/manifests`
+there are kubernetes manifests for it in the `infra` repository under `kube/manifests/gifinator`
--- rundocker.sh
+++ rundocker.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -x
 
-docker run --rm --name minio -p 9000:9000 -v /tmp/docker_data/minio/data:/data -e MINIO_ACCESS_KEY=minioaccesskeyid -e MINIO_SECRET_KEY=miniosecretaccesskey minio/minio:latest server /data &
-docker run --rm --name redis -p 6379:6379 redis:6 &
-docker run --rm --name render -p 8080:8080 inherently/gifinator:0.0.1 /render &
-docker run --rm --name gifcreator-server -p 8081:8081 -e REDIS_NAME=redis -e REDIS_PORT=6379 -e GIFCREATOR_PORT=8081 inherently/gifinator:0.0.1 /gifcreator &
-docker run --rm --name gifcreator-worker -p 8082:8082 -e REDIS_NAME=redis -e REDIS_PORT=6379 -e GIFCREATOR_PORT=8082 -e RENDER_NAME=render -e RENDER_PORT=8080 inherently/gifinator:0.0.1 /gifcreator -worker &
-docker run --rm --name frontend -p 8090:8090 -e GIFCREATOR_NAME=gifcreator-server -e GIFCREATOR_PORT=8082 inherently/gifinator:0.0.1 /frontend &
+docker network create --driver bridge gifnet
+docker run --network gifnet --rm --name redis -p 6379:6379 redis:6 &
+sleep 5s;
+docker run --network gifnet --rm --name minio -p 9000:9000 -v /tmp/docker_data/minio/data:/data -e MINIO_ACCESS_KEY=minioaccesskeyid -e MINIO_SECRET_KEY=miniosecretaccesskey minio/minio:latest server /data &
+sleep 5s;
+docker run --network gifnet --rm --name render -p 8080:8080 inherently/gifinator:0.0.1 /render &
+sleep 5s;
+docker run --network gifnet --rm --name gifcreator-server -p 8081:8081 -e MINIO_NAME=minio -e REDIS_NAME=redis -e REDIS_PORT=6379 -e RENDER_NAME=render -e RENDER_PORT=8080 -e GIFCREATOR_PORT=8082 inherently/gifinator:0.0.1 /gifcreator &
+sleep 5s;
+docker run --network gifnet --rm --name gifcreator-worker -p 8082:8082 -e MINIO_NAME=minio -e REDIS_NAME=redis -e REDIS_PORT=6379 -e GIFCREATOR_PORT=8081 -e RENDER_NAME=render -e RENDER_PORT=8080 inherently/gifinator:0.0.1 /gifcreator -worker &
+sleep 5s;
+docker run --network gifnet --rm --name frontend -p 8090:8090 -e GIFCREATOR_NAME=gifcreator-server -e GIFCREATOR_PORT=8081 -e FRONTEND_TEMPLATES_DIR=/templates inherently/gifinator:0.0.1 /frontend &
